3dff2d6e27221fd216d857b931b6a2b36ed6ca3b
anvytran-dev <anvytran857@gmail.com>
2022-01-11 19:38:26 -0500

    updated functions

--- main.py
+++ main.py
@@ -1,56 +1,103 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import requests
 
 def get_current_weather():
 
     # Collect the input (city name) from the user.
     city = input("What city do you want to do know the weather for?\n")
-    state_code = input("If you're in the US, what is the state abbreviation? If not, click Enter.\n")
-    country_code = input("What is the country name?\n")
 
-    weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{state_code},{country_code}&units=imperial&appid=e89c759b0eeedfb883f2c032ba235e16"
-    print(weather_url)
+    # Collect the input (state name) from the user (if in the US).
+    state = input("If you're in the US, what is the state abbreviation? If not, click Enter.\n")
 
+    # Collect the input (country name) from the user.
+    country = input("What is the country name?\n")
+
+    # API link
+    weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&units=imperial&appid=e89c759b0eeedfb883f2c032ba235e16"
+
+    # API REQUEST
     data = requests.get(weather_url)
+
+    # Store response received from the API request as JSON data
     results = data.json()
 
-    print(f"Location Name: {results['name']}, Country Name: {results['sys']['country']}\n Temperature: {results['main']['temp']}, Description: {results['weather'][0]['description']}")
+    #How do we use .get for more chaining keys?
+    print(results.get('name'))
 
+    # Print the location name, country name, temperature, and weather description
+    print(f"Location Name: {results.get('name')}, Country Name: {results['sys']['country']}\n Temperature: {results['main']['temp']}, Description: {results['weather'][0]['description']}")
+
+    # Continue or quit
+    continue_or_quit = input("Enter 'A' to return to the menu or Enter 'B' to quit.")
+
+    continue_or_quit = continue_or_quit.lower()
+
+    # if continue_or_quit != "a" or "b":
+        #"Invalid response. Enter 1 to return to the menu or Enter 2 to quit."
+
+    if continue_or_quit == "a":
+        get_menu()
+    elif continue_or_quit == "b":
+        exit()
+
+# Function that gets a 5-day forecast at 3-hour intervals
 def get_forcast():
 
     # Collect the input (city name) from the user.
     city = input("What city do you want to do know the weather for?\n")
+
+    # Collect the input (state name) from the user (if in the US).
     state = input("If you're in the US, what is the state abbreviation? If not, click Enter.\n")
+
+    # Collect the input (country name) from the user.
     country = input("What is the country name?\n")
 
+    # API link
     forcast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city},{state},{country}&units=imperial&appid=caab8d07c64bfd5f209298466bc24262"
 
+    # API REQUEST
     data = requests.get(forcast_url)
+
+    # Store response received from the API request as JSON data
     results = data.json()
-    print(results)
-    print(results['list'])
+
+    # Array to store temperatures
     date_forecast = []
 
+    # Loop to get the dates, temperature, and weather description for the 5-day weather forecast at 3-hour intervals
     for forcast in results['list']:
         date_forecast.append(forcast['dt_txt'])
         print(f"Date: {forcast['dt_txt']} Temperature: {forcast['main'].get('temp')}, Description: {forcast['weather'][0].get('description')}")
 
+    # Continue or quit
+    continue_or_quit = input("Enter 'A' to return to the menu or Enter 'B' to quit.")
+
+    continue_or_quit = continue_or_quit.lower()
+
+    # if continue_or_quit != "a" or "b":
+        #"Invalid response. Enter 1 to return to the menu or Enter 2 to quit."
+
+    if continue_or_quit == "a":
+        get_menu()
+    elif continue_or_quit == "b":
+        exit()
+
+def get_menu():
+
+    # What is the user's choice: current weather or 5-day forecast?
+    user_choice = input("Do you want to find the current weather(A) or get the 5 day forecast(B)? Enter 'A' or 'B'")
+
+    user_choice = user_choice.lower()
+
+    if user_choice == 'a':
+        get_current_weather()
+    elif user_choice == 'b':
+        get_forcast()
 
 def main():
     # This is the welcome message.
     print("Hello! Welcome to City Weather Finder.")  # Welcome message in ASCII -- extra
 
-    # What is the user's choice?
-    user_choice = input("Do you want to find the current weather(A) or get the 4 day forcast(B)? Enter 'A' or 'B'")
-
-    if user_choice == 'A':
-        get_current_weather()
-    elif user_choice == 'B':
-        get_forcast()
+    get_menu()
 
 
 
